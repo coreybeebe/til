02f799cbdf7110b1bab7f3aaa0cb516625b5f73a
corey beebe <corey@Hyperion.fios-router.home>
2018-06-19 21:47:15 -0400

    Reinstated password environment variable in DB config

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -22,14 +22,14 @@ public func configure (_ config: inout Config, _ env: inout Environment, _ servi
     let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
     let username = Environment.get("DATABASE_USER") ?? "vapor"
     let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-    //let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
     
     let databaseConfig = PostgreSQLDatabaseConfig (
         
         hostname: hostname,
         username: username,
-        database: databaseName
-        //password: password
+        database: databaseName,
+        password: password
     )
     
     let postgreSQLDatabase = PostgreSQLDatabase(config: databaseConfig)
